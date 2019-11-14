7c06fe4af4c50e59bccf30a2c60c43678b9fa3f9
Pascal Mathis <mail@pascalmathis.com>
2019-11-14 22:17:01 +0100

    *: Support version tags in Docker image builder

--- docker-image.mk
+++ docker-image.mk
@@ -3,20 +3,70 @@ ifndef DOCKER_IMAGE_NAME
 $(error missing required variable: DOCKER_IMAGE_NAME)
 endif
 
+# Detect latest Git tag for image
+BUILD_DATE := $(shell date -u +'%Y-%m-%dT%H:%M:%SZ')
+BUILD_TAG := $(strip $(shell git describe --abbrev=0 --match "$(DOCKER_IMAGE_NAME)/*" --tags 2>&- || echo "v0.0.0"))
+BUILD_VERSION := $(subst $(DOCKER_IMAGE_NAME)/,,$(BUILD_TAG))
+GIT_COMMIT := $(strip $(shell git rev-parse HEAD))
+GIT_COMMIT_SHORT := $(strip $(shell git rev-parse --short HEAD))
+GIT_COMMIT_DATE := $(strip $(shell git show --no-patch --format='%ct' "$(GIT_COMMIT)"))
+
 # Declare build variables
+DOCKER_REGISTRY := https://quay.io
+DOCKER_IMAGE_PATH := quay.io/snapserv/$(DOCKER_IMAGE_NAME)
+DOCKER_IMAGE_TAG := $(BUILD_VERSION)-$(GIT_COMMIT_DATE)-$(GIT_COMMIT_SHORT)
 DOCKER_LABEL_VENDOR := SnapServ
 DOCKER_LABEL_URL := https://snapserv.net
 DOCKER_LABEL_VCS_URL := https://github.com/snapserv/docker
-
-DOCKER_REGISTRY := https://quay.io
-DOCKER_IMAGE_PATH := quay.io/snapserv/$(DOCKER_IMAGE_NAME)
-DOCKER_BUILD_TAG := latest
 DOCKER_BUILD_FLAGS ?=
 
+# Check if working directory is dirty
+GIT_CLEAN_REPO_CHECK := $(strip $(shell git status --porcelain))
+GIT_CLEAN_REPO_CHECK := $(if $(firstword $(GIT_CLEAN_REPO_CHECK)),no,yes)
+ifneq ($(GIT_CLEAN_REPO_CHECK),yes)
+DOCKER_IMAGE_TAG := $(DOCKER_IMAGE_TAG)-dirty
+endif
+
+# Mark as release if current commit matches build tag commit
+GIT_TAG_COMMIT := $(strip $(shell git rev-list "$(BUILD_TAG)" --max-count=1 2>&-))
+ifeq ($(GIT_COMMIT),$(GIT_TAG_COMMIT))
+$(info producing release: current commit [$(GIT_COMMIT)] matches tag commit [$(GIT_TAG_COMMIT)])
+RELEASE_GOAL_CHECK := yes
+else
+$(info skipping release: current commit [$(GIT_COMMIT)] does not match tag commit [$(GIT_TAG_COMMIT)])
+RELEASE_GOAL_CHECK := no
+endif
+
+# Handle special logic for release targets
+ifeq ($(RELEASE_GOAL_CHECK),yes)
+## Abort if working directory is dirty
+ifneq ($(GIT_CLEAN_REPO_CHECK),yes)
+$(error unable to build release: working directory is dirty)
+endif
+
+## Modify image tag to build version
+DOCKER_IMAGE_TAG := $(subst $(DOCKER_IMAGE_NAME)/,,$(BUILD_VERSION))
+endif
+
+# Print information about current build task
+$(info ============================== $(DOCKER_IMAGE_NAME) ==============================)
+$(info >> Build Identifier: $(BUILD_VERSION) @ $(BUILD_DATE))
+$(info >> Git Commit: $(GIT_COMMIT) @ $(GIT_COMMIT_DATE))
+$(info >> Docker Image: $(DOCKER_IMAGE_PATH):$(DOCKER_IMAGE_TAG))
+$(info >> Flags: cleanRepo=$(GIT_CLEAN_REPO_CHECK) releaseGoal=$(RELEASE_GOAL_CHECK))
+$(info )
+
 # Combined targets
-default: build output
-release: test push output
-test: lint build
+default: test
+release: test push
+test: lint build output
+
+# Automated CI target (release or test)
+ifeq ($(RELEASE_GOAL_CHECK),yes)
+auto: release
+else
+auto: test
+endif
 
 # Build container image
 build:
@@ -25,8 +75,13 @@ build:
 		--label org.label-schema.vendor="$(DOCKER_LABEL_VENDOR)" \
 		--label org.label-schema.url="$(DOCKER_LABEL_URL)" \
 		--label org.label-schema.vcs-url="$(DOCKER_LABEL_VCS_URL)" \
+		--label org.label-schema.build-date="$(BUILD_DATE)" \
+		--label org.label-schema.version="$(BUILD_VERSION)" \
+		--label org.label-schema.vcs-ref="$(GIT_COMMIT)" \
 		--label org.label-schema.schema-version="1.0" \
-		--tag $(DOCKER_IMAGE_PATH):$(DOCKER_BUILD_TAG) .
+		--tag $(DOCKER_IMAGE_PATH):latest \
+		--tag $(DOCKER_IMAGE_PATH):$(DOCKER_IMAGE_TAG) \
+		./
 
 # Lint dockerfile
 lint:
@@ -41,10 +96,11 @@ login:
 
 # Push image to registry
 push: login
-	docker push $(DOCKER_IMAGE_PATH):$(DOCKER_BUILD_TAG)
+	docker push $(DOCKER_IMAGE_PATH):$(DOCKER_IMAGE_TAG)
+	docker push $(DOCKER_IMAGE_PATH):latest
 
 # Output image name
 output:
-	@echo Docker Image: $(DOCKER_IMAGE_PATH):$(DOCKER_BUILD_TAG)
+	@echo Docker Image: $(DOCKER_IMAGE_PATH):$(DOCKER_IMAGE_TAG)
 
-.PHONY: default release test build lint login push output
+.PHONY: default release test build lint login push output auto
