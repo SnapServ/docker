102fdf93b7a317aba8e23ffd9a92723fe41d3dbc
Pascal Mathis <mail@pascalmathis.com>
2019-11-16 22:01:58 +0100

    hugo: Fix and extend Goss tests

--- hugo/rootfs/docker-entrypoint.sh
+++ hugo/rootfs/docker-entrypoint.sh
@@ -4,10 +4,17 @@ set -euo pipefail
 . /usr/local/lib/scp
 
 scp_prepare_dir "/tmp/hugo" "hugo:hugo" "0700"
+scp_prepare_dir "/tmp/hugo-quickstart" "hugo:hugo" "0700"
 
 if [ $# -ge 1 ]; then
     scp_runas "hugo" hugo "${@}"
 else
+    if [ ! -f /usr/src/app/config.toml ]; then
+        scp_warn "no config.toml in /usr/src/app found, generating empty quickstart page..."
+        cd /tmp/hugo-quickstart
+        ( scp_runas "hugo" hugo new site . )
+    fi
+
     scp_runas "hugo" hugo server \
         --bind=:: \
         --cacheDir=/tmp/hugo \
