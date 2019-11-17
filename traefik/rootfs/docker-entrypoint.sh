6f78f34cdf236470d5c15159c6d6a59e03ca41f8
Pascal Mathis <mail@pascalmathis.com>
2019-11-17 03:03:13 +0100

    feat(traefik): switch to CLI-based config

--- traefik/rootfs/docker-entrypoint.sh
+++ traefik/rootfs/docker-entrypoint.sh
@@ -6,4 +6,9 @@ set -euo pipefail
 scp_prepare_dir "/cts/traefik/config" "traefik:traefik" "0700"
 scp_prepare_dir "/cts/traefik/data" "traefik:traefik" "0700"
 
-scp_runas "traefik" traefik "${@}"
+scp_runas "traefik" traefik \
+    --entryPoints.ping.address=":8081" \
+    --ping.entryPoint="ping" \
+    --providers.file.directory="/cts/traefik/config" \
+    --providers.file.watch=true \
+    "${@}"
