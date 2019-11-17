37b3ef09ab73473e0270ac7c3a35fbdf318e7847
Pascal Mathis <mail@pascalmathis.com>
2019-11-17 02:12:06 +0100

    feat(traefik): Support file-based configs

--- traefik/rootfs/docker-entrypoint.sh
+++ traefik/rootfs/docker-entrypoint.sh
@@ -3,4 +3,7 @@
 set -euo pipefail
 . /usr/local/lib/scp
 
+scp_prepare_dir "/cts/traefik/config" "traefik:traefik" "0700"
+scp_prepare_dir "/cts/traefik/data" "traefik:traefik" "0700"
+
 scp_runas "traefik" traefik "${@}"
