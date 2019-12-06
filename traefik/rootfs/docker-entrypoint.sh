afedf68686df611119f030fba78c75217a98b1f6
Pascal Mathis <mail@pascalmathis.com>
2019-12-06 23:23:28 +0100

    feat(traefik): skip default args with custom args

--- traefik/rootfs/docker-entrypoint.sh
+++ traefik/rootfs/docker-entrypoint.sh
@@ -6,9 +6,12 @@ set -euo pipefail
 scp_prepare_dir "/cts/traefik/config" "traefik:traefik" "0700"
 scp_prepare_dir "/cts/traefik/data" "traefik:traefik" "0700"
 
-scp_runas "traefik" traefik \
-    --api --ping \
-    --entryPoints.traefik.address=":8081" \
-    --providers.file.directory="/cts/traefik/config" \
-    --providers.file.watch=true \
-    "${@}"
+if [ "${#}" -ge 1 ]; then
+    scp_runas "traefik" traefik "${@}"
+else
+    scp_runas "traefik" traefik \
+        --api=true --ping=true \
+        --entryPoints.traefik.address=":8081" \
+        --providers.file.directory="/cts/traefik/config" \
+        --providers.file.watch=true
+fi
