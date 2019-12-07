3890c165b42ec3ff9fd1f8c7f2e2611acc6c5c0e
Pascal Mathis <mail@pascalmathis.com>
2019-12-07 21:46:31 +0100

    nginx: switch to non-https configuration

--- nginx/rootfs/docker-entrypoint.sh
+++ nginx/rootfs/docker-entrypoint.sh
@@ -5,6 +5,5 @@ set -euo pipefail
 
 scp_prepare_dir "/run/nginx" "nginx:nginx" "0700"
 scp_prepare_dir "/tmp/nginx" "nginx:nginx" "0700"
-scp_certificate "nginx:nginx" "/run/nginx/ssl.crt" "/run/nginx/ssl.key"
 
 scp_runas nginx "nginx" -g "daemon off;"
