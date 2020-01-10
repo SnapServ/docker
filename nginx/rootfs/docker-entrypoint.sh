a65508512e914f5891ecbf5d15056b55d21d83b5
Pascal Mathis <mail@pascalmathis.com>
2020-01-10 23:06:27 +0100

    feat(nginx): add non-root support

--- nginx/rootfs/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#shellcheck shell=ash
-set -euo pipefail
-. /usr/local/lib/scp
-
-scp_prepare_dir "/run/nginx" "nginx:nginx" "0700"
-scp_prepare_dir "/tmp/nginx" "nginx:nginx" "0700"
-
-scp_runas nginx "nginx" -g "daemon off;"
