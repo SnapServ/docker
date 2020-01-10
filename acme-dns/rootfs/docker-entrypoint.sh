da5455c1bf8cd1b39df87dfdc284b7125877dbe2
Pascal Mathis <mail@pascalmathis.com>
2020-01-10 23:35:47 +0100

    feat(acme-dns): update storage schema

--- acme-dns/rootfs/docker-entrypoint.sh
+++ acme-dns/rootfs/docker-entrypoint.sh
@@ -2,8 +2,10 @@
 #shellcheck shell=ash
 set -euo pipefail
 
-ctutil template /etc/acme-dns/config.tmpl=/cts/acme-dns/acme-dns.cfg
+ctutil template /etc/acme-dns/config.tmpl=/cts/acme-dns/volatile/acme-dns.cfg
 
-exec /usr/local/bin/acme-dns \
-    -c "/cts/acme-dns/acme-dns.cfg" \
+ls -la /cts/acme-dns/volatile
+
+exec ctutil run -p acme-dns -- /usr/local/bin/acme-dns \
+    -c "/cts/acme-dns/volatile/acme-dns.cfg" \
     "${@}"
