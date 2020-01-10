5894966f96950c06a2c7f9350aa6ab72aef89e6c
Pascal Mathis <mail@pascalmathis.com>
2020-01-11 00:17:18 +0100

    feat(hugo): add non-root support

--- hugo/rootfs/docker-entrypoint.sh
+++ hugo/rootfs/docker-entrypoint.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
 #shellcheck shell=ash
 set -euo pipefail
-. /usr/local/lib/scp
-
-scp_prepare_dir "/tmp/hugo" "hugo:hugo" "0700"
-scp_prepare_dir "/tmp/hugo-quickstart" "hugo:hugo" "0700"
 
 if [ $# -ge 1 ]; then
-    scp_runas "hugo" hugo "${@}"
+    exec ctutil run -p hugo -- hugo "${@}"
 else
-    if [ ! -f /usr/src/app/config.toml ]; then
-        scp_warn "no config.toml in /usr/src/app found, generating empty quickstart page..."
-        cd /tmp/hugo-quickstart
-        ( scp_runas "hugo" hugo new site . )
+    if [ ! -f /cts/hugo/persistent/data/config.toml ]; then
+        ctutil log "no config.toml found, generating empty quickstart page..."
+        ctutil run -p hugo -- hugo new site .
     fi
 
-    scp_runas "hugo" hugo server \
+    exec ctutil run -p hugo -- hugo server \
         --bind=:: \
-        --cacheDir=/tmp/hugo \
+        --cacheDir=/cts/hugo/volatile/tmp \
         --config=/etc/hugo-docker.toml,config.toml
 fi
