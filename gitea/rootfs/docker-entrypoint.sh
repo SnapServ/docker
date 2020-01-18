d69f270c9ae0d3b098715f0b1a607cb3906b207a
Pascal Mathis <mail@pascalmathis.com>
2020-01-18 21:52:27 +0100

    fix(gitea): create missing directories on startup

--- gitea/rootfs/docker-entrypoint.sh
+++ gitea/rootfs/docker-entrypoint.sh
@@ -2,6 +2,9 @@
 #shellcheck shell=ash
 set -euo pipefail
 
+ctutil directory -u gitea -g gitea -m 0700 \
+  /cts/gitea/persistent/data \
+  /cts/gitea/persistent/home
 ctutil template /etc/gitea/app.ini.tmpl=/cts/gitea/persistent/data/app.ini
 
 exec ctutil run -p gitea -- gitea web \
