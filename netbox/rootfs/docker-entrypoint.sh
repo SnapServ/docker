4bbe3fce053165af9c9fb8e800cde53762b4db57
Pascal Mathis <mail@pascalmathis.com>
2020-02-02 18:21:36 +0100

    feat(netbox): add new image

---
+++ netbox/rootfs/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#shellcheck shell=ash
+set -euo pipefail
+
+ctutil directory -m 0700 \
+  /cts/netbox/persistent/media
+ctutil template /etc/netbox/configuration.py.tmpl=/cts/netbox/persistent/configuration.py
+
+exec ctutil run -p netbox -- /opt/netbox/venv/bin/gunicorn \
+  --bind '[::]:8080' \
+  --pythonpath '/opt/netbox/netbox' \
+  --worker-class 'gthread' \
+  --workers 2 \
+  --threads 4 \
+  --forwarded-allow-ips '*' \
+  --access-logfile - \
+  --error-logfile - \
+  netbox.wsgi
