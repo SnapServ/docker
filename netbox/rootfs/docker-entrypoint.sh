55149c5dfdb3a8ab25809178238ec23bc06de667
Pascal Mathis <mail@pascalmathis.com>
2020-02-07 17:52:24 +0100

    feat(netbox): support running custom commands

--- netbox/rootfs/docker-entrypoint.sh
+++ netbox/rootfs/docker-entrypoint.sh
@@ -2,11 +2,18 @@
 #shellcheck shell=ash
 set -euo pipefail
 
+. /opt/netbox/venv/bin/activate
+
 ctutil directory -m 0700 \
   /cts/netbox/persistent/media
 ctutil template /etc/netbox/configuration.py.tmpl=/cts/netbox/persistent/configuration.py
 
-exec ctutil run -p netbox -- /opt/netbox/venv/bin/gunicorn \
+if [ $# -ge 1 ]; then
+  cd /opt/netbox/netbox
+  exec ctutil run -p netbox -- "${@}"
+fi
+
+exec ctutil run -p netbox -- gunicorn \
   --bind '[::]:8080' \
   --pythonpath '/opt/netbox/netbox' \
   --worker-class 'gthread' \
