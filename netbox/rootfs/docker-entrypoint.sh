70adb3b1ad5ab09d064785241bfeb937c2aee7e0
Pascal Mathis <mail@pascalmathis.com>
2020-02-07 18:41:53 +0100

    feat(netbox): run args with manage.py

--- netbox/rootfs/docker-entrypoint.sh
+++ netbox/rootfs/docker-entrypoint.sh
@@ -2,18 +2,16 @@
 #shellcheck shell=ash
 set -euo pipefail
 
-. /opt/netbox/venv/bin/activate
-
 ctutil directory -m 0700 \
   /cts/netbox/persistent/media
 ctutil template /etc/netbox/configuration.py.tmpl=/cts/netbox/persistent/configuration.py
 
 if [ $# -ge 1 ]; then
-  cd /opt/netbox/netbox
-  exec ctutil run -p netbox -- "${@}"
+  exec ctutil run -p netbox -- /opt/netbox/venv/bin/python \
+    /opt/netbox/netbox/manage.py "${@}"
 fi
 
-exec ctutil run -p netbox -- gunicorn \
+exec ctutil run -p netbox -- /opt/netbox/venv/bin/gunicorn \
   --bind '[::]:8080' \
   --pythonpath '/opt/netbox/netbox' \
   --worker-class 'gthread' \
